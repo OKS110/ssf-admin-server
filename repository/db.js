59822987d47fe2f6160a1e820120105b38bee15e
oks110 <seokko12@naver.com>
2025-03-26 16:36:29 +0900

    commit

--- repository/db.js
+++ repository/db.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'ssf-react-db.c70maeu0qcke.ap-northeast-2.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
+    user: 'ohkwangseok',
     password: 'mysql1234',
     database: 'shopping_mall'
 });
